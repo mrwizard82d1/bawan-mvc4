5770acee2e767625e18822add87f678d5d077a02
Lawrence Allan Jones <lawrence.jones@openlink.com>
2016-07-30 14:13:41 -0500

    Add new route/controller
    Added new route and controller to support searching by cuisine.

--- OdeTofood/App_Start/RouteConfig.cs
+++ OdeTofood/App_Start/RouteConfig.cs
@@ -13,6 +13,18 @@ namespace OdeTofood
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // /Cuisine/french
+
+            // If we allow the user to enter routes like the previous route, we would have an issue: every cuisine, 
+            // like french, would require us to implement an appropriately named action method. Although this approach
+            // is doable, it would be better to treat the last component of the URL as a parameter rather than an action.
+            //
+            // Because the default route matches the preceeding route, we must map this route using different 
+            // information. Because the routing engine looks at candidate routes using the routes in the order defined,
+            // we must create the new route **before** the default route.
+
+            routes.MapRoute("Cuisine", "Cuisine/{name}", new {controller = "Cuisine", action = "Search", name = ""});
+
             // /Home
 
             routes.MapRoute(
