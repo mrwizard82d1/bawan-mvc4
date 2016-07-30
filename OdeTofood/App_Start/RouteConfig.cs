a8ae19031b6b63517513c7b03df2effe2d71e0ee
Lawrence Allan Jones <lawrence.jones@openlink.com>
2016-07-30 13:54:22 -0500

    Display route information
    Change `HomeController.Index` to display information about the route
    chosen by the routing engine to arrive at the `HomeController` on the
    `Index` page.

--- OdeTofood/App_Start/RouteConfig.cs
+++ OdeTofood/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace OdeTofood
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // /Home
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
