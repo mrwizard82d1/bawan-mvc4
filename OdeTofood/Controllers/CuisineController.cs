ab969f6cbd5386e0bb78371b79aefb6006fca4d6
Lawrence Allan Jones <lawrence.jones@openlink.com>
2016-07-30 14:18:16 -0500

    Add name parameter to action.
    Add the `name` parameter to the `Search` action of the `Cuisine`
    controller. To verify that we correctly handle this parameter, return
    the (HTML encoded) content with that name.

--- OdeTofood/Controllers/CuisineController.cs
+++ OdeTofood/Controllers/CuisineController.cs
@@ -11,9 +11,12 @@ namespace OdeTofood.Controllers
         //
         // GET: /Cuisine/
 
-        public ActionResult Search()
+        public ActionResult Search(string name)
         {
-            return Content("Hello!");
+            // Perform HTML encoding to prevent a cross-site scripting attack. (Very, very bad to send raw content to
+            // the browser.
+            var message = Server.HtmlEncode(name);
+            return Content(string.Format("Hello, {0}!", name));
         }
 
     }
