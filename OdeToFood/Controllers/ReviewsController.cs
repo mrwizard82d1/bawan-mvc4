23e23fdcdcf64c738c92693f4cd43a5eeda2f954
Lawrence Allan Jones <lawrence.jones@openlink.com>
2016-07-30 22:46:46 -0500

    Rating with scale

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -120,7 +120,7 @@ namespace OdeToFood.Controllers
             {
                 Id = 2,
                 Name = "Marrakesh",
-                City = "<script>alert('xss')</script>",
+                City = "D. C.",
                 Country = "USA",
                 Rating = 10
             },
